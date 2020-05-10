2eece1fc58bb7a616001fc1269841a5152a1700e
Beyonce Ebo <manduulya@yahoo.com>
2020-05-09 22:10:54 -0400

    patch request added

--- src/BookmarkItem/BookmarkItem.js
+++ src/BookmarkItem/BookmarkItem.js
@@ -1,36 +1,74 @@
-import React from 'react';
-import Rating from '../Rating/Rating';
-import './BookmarkItem.css';
+import React from "react";
+import Rating from "../Rating/Rating";
+import "./BookmarkItem.css";
+import config from "../config";
+import BookmarksContext from "../BookmarksContext";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+function deleteBookmarkRequest(bookmarkId, cb) {
+  fetch(config.API_ENDPOINT + `'/${bookmarkId}`, {
+    method: "DELETE",
+    header: {
+      "content-type": "application/json",
+      authorization: `bearer ${config.API_KEY}`,
+    },
+  })
+    .then((res) => {
+      if (!res.ok) {
+        return res.json().then((error) => Promise.reject(error));
+      }
+    })
+    .then(() => {
+      cb(bookmarkId);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
 
 export default function BookmarkItem(props) {
   return (
-    <li className='BookmarkItem'>
-      <div className='BookmarkItem__row'>
-        <h3 className='BookmarkItem__title'>
-          <a
-            href={props.url}
-            target='_blank'
-            rel='noopener noreferrer'>
-            {props.title}
-          </a>
-        </h3>
-        <Rating value={props.rating} />
-      </div>
-      <p className='BookmarkItem__description'>
-        {props.description}
-      </p>
-      <div className='BookmarkItem__buttons'>
-        <button
-          className='BookmarkItem__description'
-          onClick={() => props.onClickDelete(props.id)}
-        >
-          Delete
-        </button>
-      </div>
-    </li>
-  )
+    <BookmarksContext.Consumer>
+      {(context) => (
+        <li className="BookmarkItem">
+          <div className="BookmarkItem__row">
+            <h3 className="BookmarkItem__title">
+              <a href={props.url} target="_blank" rel="noopener noreferrer">
+                {props.title}
+              </a>
+            </h3>
+            <Rating value={props.rating} />
+          </div>
+          <p className="BookmarkItem__description">{props.description}</p>
+          <div className="BookmarkItem__buttons">
+            <Link to={`/edit/${props.id}`}>
+              <button>Edit</button>
+            </Link>
+            <button
+              className="BookmarkItem__description"
+              onClick={() =>
+                deleteBookmarkRequest(props.id, context.deleteBookmark)
+              }
+            >
+              Delete
+            </button>
+          </div>
+        </li>
+      )}
+    </BookmarksContext.Consumer>
+  );
 }
 
 BookmarkItem.defaultProps = {
   onClickDelete: () => {},
-}
+};
+
+BookmarkItem.propTypes = {
+  id: PropTypes.oneOfType([PropTypes.number, PropTypes.string]).isRequired,
+  title: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
+  description: PropTypes.string,
+  rating: PropTypes.number.isRequired,
+  onClickDelete: PropTypes.func,
+};
